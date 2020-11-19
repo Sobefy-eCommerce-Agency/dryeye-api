b2da30579671ab1b37d6f9f36f1dbf513389f96d
Bernardo <ber.montoya12@gmail.com>
2020-11-19 01:02:21 -0600

    feat: add service to create a doctor

---
+++ handlers/doctors/createDoctor.js
@@ -0,0 +1,32 @@
+import * as uuid from "uuid";
+import handler from "../../libs/doctors-handler-lib";
+import dynamoDb from "../../libs/dynamodb-lib";
+
+export const main = handler(async (event) => {
+  const data = JSON.parse(event.body);
+  const {
+    email,
+    phone,
+    first_name,
+    last_name,
+    practice_name,
+    verified_customer,
+  } = data;
+  const params = {
+    TableName: process.env.doctorsTable,
+    Item: {
+      email,
+      phone,
+      first_name,
+      last_name,
+      verified_customer,
+      note: practice_name,
+      doctor: uuid.v1(),
+      created_at: Date.now(),
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return { status: 200 };
+});

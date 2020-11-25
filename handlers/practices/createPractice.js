f0698ca0211e5aae35247b9cdaac81f1bf2018f9
Bernardo <ber.montoya12@gmail.com>
2020-11-25 01:22:09 -0600

    feat: add service to create practices

---
+++ handlers/practices/createPractice.js
@@ -0,0 +1,27 @@
+import * as uuid from "uuid";
+import handler from "../../libs/doctors-handler-lib";
+import dynamoDb from "../../libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const { name, phone, address, address2, city, state, stateName, zip } = data;
+  const params = {
+    TableName: process.env.practices_table,
+    Item: {
+      name,
+      phone,
+      address,
+      address2,
+      city,
+      state,
+      stateName,
+      zip,
+      practice: uuid.v1(),
+      createdAt: Date.now(),
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return { status: 200 };
+});
--- libs/doctors-handler-lib.js
+++ libs/doctors-handler-lib.js
@@ -19,7 +19,7 @@ export default function handler(lambda) {
       statusCode,
       body: JSON.stringify(body),
       headers: {
-        "Access-Control-Allow-Origin": "https://store.dryeyerescue.com",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": true,
       },
     };

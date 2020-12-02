ae418edceacf126bed49253db8216cb2d751588e
Bernardo <ber.montoya12@gmail.com>
2020-12-02 02:06:58 -0600

    feat: remove practice id of the doctor when a practice is deleted

--- handlers/practices/deletePractice.js
+++ handlers/practices/deletePractice.js
@@ -5,7 +5,7 @@ export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
   const { practice, doctor } = data;
 
-  const params = {
+  const deletePracticeParams = {
     TableName: process.env.practices_table,
     Key: {
       practice,
@@ -13,7 +13,63 @@ export const main = handler(async (event) => {
     },
   };
 
-  await dynamoDb.delete(params);
+  // Get all available doctors
+  const allDoctorsParams = {
+    TableName: process.env.doctors_table,
+    ExpressionAttributeNames: {
+      "#n": "first_name",
+      "#ln": "last_name",
+      "#d": "doctor",
+      "#p": "practices",
+      "#e": "email",
+    },
+    Select: "SPECIFIC_ATTRIBUTES",
+    ProjectionExpression: "#n,#ln,#d,#p,#e",
+  };
+  const allDoctors = await dynamoDb.scan(allDoctorsParams);
+
+  // Filter the doctors with the current practice
+  const filteredDoctors = [];
+  if (allDoctors.Items) {
+    for (let i = 0; i < allDoctors.Items.length; i++) {
+      const currentDoctor = allDoctors.Items[i];
+      const currentDoctorPractices = currentDoctor.practices;
+      const includesPractice = currentDoctorPractices
+        ? currentDoctorPractices.includes(practice)
+        : false;
+      if (includesPractice) {
+        filteredDoctors.push(currentDoctor);
+      }
+    }
+  }
+
+  // update the current practices of the doctor
+  if (filteredDoctors.length > 0) {
+    for (let i = 0; i < filteredDoctors.length; i++) {
+      const currentFilteredDoctor = filteredDoctors[i];
+      const formattedPractices = currentFilteredDoctor?.practices
+        ? currentFilteredDoctor.practices
+        : [];
+      const filteredPractices = formattedPractices.filter(
+        (currentPractice) => currentPractice !== practice
+      );
+      const updateDoctorPractices = {
+        TableName: process.env.doctors_table,
+        Key: {
+          doctor: currentFilteredDoctor.doctor,
+        },
+        UpdateExpression: "SET practices = :practices",
+        ExpressionAttributeValues: {
+          ":practices": filteredPractices,
+        },
+        ReturnValues: "NONE",
+      };
+      await dynamoDb.update(updateDoctorPractices);
+    }
+  }
+
+  // delete the corresponding practice
+  await dynamoDb.delete(deletePracticeParams);
 
   return { status: 200 };
 });

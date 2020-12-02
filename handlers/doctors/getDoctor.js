83ce07b77172d3f8fc42d1302831c4acf8b7d839
Bernardo <ber.montoya12@gmail.com>
2020-12-02 01:04:23 -0600

    feat: add service to filter the doctors by the current practice

--- handlers/doctors/getDoctor.js
+++ handlers/doctors/getDoctor.js
@@ -2,24 +2,40 @@ import AWS from "aws-sdk";
 import handler from "../../libs/doctors-handler-lib";
 
 export const main = handler(async (event) => {
-  const { practice, doctor } = event.queryStringParameters;
-  const params = {
+  const { practice } = event.queryStringParameters;
+  const allDoctorsParams = {
     TableName: process.env.doctors_table,
     ExpressionAttributeNames: {
       "#n": "first_name",
       "#ln": "last_name",
       "#d": "doctor",
+      "#p": "practices",
+      "#e": "email",
     },
     Select: "SPECIFIC_ATTRIBUTES",
-    ProjectionExpression: "#n,#ln,#d",
-    FilterExpression: practice ? "practice = :practice" : "doctor = :doctor",
-    ExpressionAttributeValues: { ":practice": practice, ":doctor": doctor },
+    ProjectionExpression: "#n,#ln,#d,#p,#e",
   };
 
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  const results = await dynamoDb.scan(params).promise();
+  // Get all available doctors
+  const allDoctors = await dynamoDb.scan(allDoctorsParams).promise();
+
+  // Filter the doctors with the current practice
+  const filteredDoctors = [];
+  if (allDoctors.Items) {
+    for (let i = 0; i < allDoctors.Items.length; i++) {
+      const currentDoctor = allDoctors.Items[i];
+      const currentDoctorPractices = currentDoctor.practices;
+      const includesPractice = currentDoctorPractices
+        ? currentDoctorPractices.includes(practice)
+        : false;
+      if (includesPractice) {
+        filteredDoctors.push(currentDoctor);
+      }
+    }
+  }
 
   // Return the retrieved item
-  return results;
+  return filteredDoctors;
 });

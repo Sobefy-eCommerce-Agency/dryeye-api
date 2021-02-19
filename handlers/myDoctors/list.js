2a6f6ae59629ec6d4ce1146ad66ee38b8ff4d054
Bernardo <ber.montoya12@gmail.com>
2021-02-19 08:47:11 -0600

    fix: only list doctors with the doctor tag

--- handlers/myDoctors/list.js
+++ handlers/myDoctors/list.js
@@ -2,7 +2,7 @@ import handler from "../../libs/handler-lib";
 import dynamoDb from "../../libs/dynamodb-lib";
 
 export const main = handler(async (event) => {
-  const { customer, owner } = event.queryStringParameters;
+  const { customer } = event.queryStringParameters;
 
   const params = {
     TableName: process.env.my_doctors_table,
@@ -18,21 +18,34 @@ export const main = handler(async (event) => {
         filteredDoctors.push(currentDoctor);
       }
     }
-    if (owner === "true") {
-      const params = {
-        TableName: process.env.doctors_table,
-        Key: {
-          doctor: customer,
-        },
-      };
-      const currentOwner = await dynamoDb.get(params);
-      if (currentOwner.Item) {
-        // add the owner as a doctor
-        filteredDoctors.push({
-          firstName: currentOwner.Item.first_name,
-          lastName: currentOwner.Item.last_name,
-          doctor: currentOwner.Item.doctor,
-        });
+    // check if the customer is owner
+    const params = {
+      TableName: process.env.doctors_table,
+      Key: {
+        doctor: customer,
+      },
+    };
+    const currentOwner = await dynamoDb.get(params);
+    if (currentOwner.Item) {
+      // check tags
+      const tags = currentOwner.Item.tags;
+      if (tags) {
+        const tagsArray = tags.split(",");
+        if (tagsArray && tagsArray.length > 0) {
+          for (let i = 0; i < tagsArray.length; i++) {
+            const currentTag = tagsArray[i];
+            const trimmedTag = currentTag.trim();
+            // check if the tag includes the Doctor tag
+            if (trimmedTag === "Type of Account|Doctor") {
+              // add the current doctor as a owner
+              filteredDoctors.push({
+                firstName: currentOwner.Item.first_name,
+                lastName: currentOwner.Item.last_name,
+                doctor: currentOwner.Item.doctor,
+              });
+            }
+          }
+        }
       }
     }
   }

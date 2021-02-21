9ba831158f89b7f55f382bed211961061354ea62
Bernardo <ber.montoya12@gmail.com>
2021-02-21 17:11:17 -0600

    fix: add practice in My Doctors entity

--- handlers/myDoctors/create.js
+++ handlers/myDoctors/create.js
@@ -4,39 +4,37 @@ import dynamoDb from "../../libs/dynamodb-lib";
 
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
-  const {
-    firstName,
-    lastName,
-    email,
-    phone,
-    birthdate,
-    address,
-    address2,
-    city,
-    state,
-    zip,
-    customer,
-  } = data;
-  const params = {
-    TableName: process.env.my_doctors_table,
-    Item: {
-      firstName,
-      lastName,
-      email,
-      phone,
-      birthdate,
-      address,
-      address2,
-      city,
-      state,
-      zip,
-      owner: customer,
-      doctor: uuid.v1(),
-      createdAt: Date.now(),
+  const { firstName, lastName, practice, customer } = data;
+
+  // Check if the customer has practices
+  const doctorParams = {
+    TableName: process.env.doctors_table,
+    Key: {
+      doctor: customer,
     },
   };
-
-  await dynamoDb.put(params);
-
-  return { status: 200 };
+  const currentDoctor = await dynamoDb.get(doctorParams);
+  if (currentDoctor.Item) {
+    const addresses = currentDoctor.Item.addresses;
+    if (addresses.length > 0) {
+      const params = {
+        TableName: process.env.my_doctors_table,
+        Item: {
+          firstName,
+          lastName,
+          practice,
+          owner: customer,
+          doctor: uuid.v1(),
+          createdAt: Date.now(),
+        },
+      };
+      await dynamoDb.put(params);
+      return { status: 200 };
+    }
+    return {
+      status: 400,
+      message: "You have not added a default practice",
+    };
+  }
+  return { status: 400, message: "An unexpected error happened" };
 });

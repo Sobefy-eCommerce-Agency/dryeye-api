3b4fcbf5cdc9b21ebc05e7b08f8be139ab354f7b
Bernardo <ber.montoya12@gmail.com>
2021-02-22 20:35:29 -0600

    fix: remove unnecesary service

--- handlers/practices/list.js
+++ handlers/practices/list.js
@@ -3,22 +3,15 @@ import dynamoDb from "../../libs/dynamodb-lib";
 
 export const main = handler(async (event) => {
   const { doctor } = event.queryStringParameters;
-
   const params = {
     TableName: process.env.practices_table,
+    KeyConditionExpression: "doctor = :doctor",
+    ExpressionAttributeValues: {
+      ":doctor": doctor,
+    },
   };
 
-  const myPractices = await dynamoDb.scan(params);
-
-  const filteredPractices = [];
-  if (myPractices.Items) {
-    for (let i = 0; i < myPractices.Items.length; i++) {
-      const currentPractice = myPractices.Items[i];
-      if (currentPractice.doctor === doctor) {
-        filteredPractices.push(currentPractice);
-      }
-    }
-  }
-
-  return filteredPractices;
+  const response = await dynamoDb.query(params);
+  const practices = response.Items;
+  return practices;
 }, true);

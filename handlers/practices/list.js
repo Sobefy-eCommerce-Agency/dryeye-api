d050d140658080ff2217db84ee8a1a81831b48c5
Bernardo <ber.montoya12@gmail.com>
2021-02-22 20:54:33 -0600

    fix: if not doctor is provided, return all practices

--- handlers/practices/list.js
+++ handlers/practices/list.js
@@ -2,16 +2,22 @@ import handler from "../../libs/handler-lib";
 import dynamoDb from "../../libs/dynamodb-lib";
 
 export const main = handler(async (event) => {
-  const { doctor } = event.queryStringParameters;
-  const params = {
-    TableName: process.env.practices_table,
-    KeyConditionExpression: "doctor = :doctor",
-    ExpressionAttributeValues: {
-      ":doctor": doctor,
-    },
-  };
-
-  const response = await dynamoDb.query(params);
+  let response = [];
+  if (event.queryStringParameters?.doctor) {
+    const params = {
+      TableName: process.env.practices_table,
+      KeyConditionExpression: "doctor = :doctor",
+      ExpressionAttributeValues: {
+        ":doctor": event.queryStringParameters.doctor,
+      },
+    };
+    response = await dynamoDb.query(params);
+  } else {
+    const params = {
+      TableName: process.env.practices_table,
+    };
+    response = await dynamoDb.scan(params);
+  }
   const practices = response.Items;
   return practices;
 }, true);

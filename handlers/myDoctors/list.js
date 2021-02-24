cfee1de561adafa35b3f84f8fc1e72c379c93ab0
Bernardo <ber.montoya12@gmail.com>
2021-02-24 08:52:51 -0600

    fix: add owner parameter

--- handlers/myDoctors/list.js
+++ handlers/myDoctors/list.js
@@ -2,20 +2,26 @@ import handler from "../../libs/handler-lib";
 import dynamoDb from "../../libs/dynamodb-lib";
 
 export const main = handler(async (event) => {
-  const { practice } = event.queryStringParameters;
-
   const params = {
     TableName: process.env.my_doctors_table,
   };
-
   const myDoctors = await dynamoDb.scan(params);
 
   const filteredDoctors = [];
   if (myDoctors.Items) {
-    for (let i = 0; i < myDoctors.Items.length; i++) {
-      const currentDoctor = myDoctors.Items[i];
-      if (currentDoctor.practice === practice) {
-        filteredDoctors.push(currentDoctor);
+    if (event.queryStringParameters?.practice) {
+      for (let i = 0; i < myDoctors.Items.length; i++) {
+        const currentDoctor = myDoctors.Items[i];
+        if (currentDoctor.practice === event.queryStringParameters.practice) {
+          filteredDoctors.push(currentDoctor);
+        }
+      }
+    } else {
+      for (let i = 0; i < myDoctors.Items.length; i++) {
+        const currentDoctor = myDoctors.Items[i];
+        if (currentDoctor.owner === event.queryStringParameters.owner) {
+          filteredDoctors.push(currentDoctor);
+        }
       }
     }
   }

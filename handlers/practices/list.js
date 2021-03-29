a3eec84f1ec852b584d7253ef37e73ffcea97b13
Bernardo <ber.montoya12@gmail.com>
2021-03-29 00:40:05 -0600

    feat: filter practices by doctor

--- handlers/practices/list.js
+++ handlers/practices/list.js
@@ -2,7 +2,7 @@ import handler from "../../libs/handler-lib";
 import dynamoDb from "../../libs/dynamodb-lib";
 
 export const main = handler(async (event) => {
-  let response = [];
+  let response = { Items: [] };
   if (event.queryStringParameters?.doctor) {
     const params = {
       TableName: process.env.practices_table,
@@ -19,7 +19,25 @@ export const main = handler(async (event) => {
     const params = {
       TableName: process.env.practices_table,
     };
-    response = await dynamoDb.scan(params);
+    const practicesResult = await dynamoDb.scan(params);
+    if (practicesResult && practicesResult.Items.length > 0) {
+      for (let i = 0; i < practicesResult.Items.length; i++) {
+        const currentPractice = practicesResult.Items[i];
+        const doctorParams = {
+          TableName: process.env.doctors_table,
+          KeyConditionExpression: "doctor = :doctor",
+          ExpressionAttributeValues: {
+            ":doctor": currentPractice.doctor,
+          },
+        };
+        const currentDoctor = await dynamoDb.query(doctorParams);
+        let doctorName = "";
+        if (currentDoctor && currentDoctor.Items.length === 1) {
+          doctorName = `${currentDoctor.Items[0].first_name} ${currentDoctor.Items[0].last_name}`;
+        }
+        response.Items.push({ ...currentPractice, doctorName });
+      }
+    }
   }
   const practices = response.Items;
   return practices;

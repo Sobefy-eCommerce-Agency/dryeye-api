3d60e083f08e0bbf4477f14a6ac9000b6dce1c75
Bernardo Montoya <ber.montoya12@gmail.com>
2022-04-15 12:10:58 -0600

    feat: add practices list for WordPress

---
+++ handlers/practices/list-wp.js
@@ -0,0 +1,152 @@
+import axios from "axios";
+import handler from "../../libs/handler-lib";
+import dynamoDb from "../../libs/dynamodb-lib";
+
+export const main = handler(async () => {
+  const params = {
+    TableName: process.env.practices_table,
+  };
+  const practicesResult = await dynamoDb.scan(params);
+  const { Items } = practicesResult;
+  let formattedItems = [...Items];
+
+  // Format Gallery URLs
+  formattedItems = formattedItems.map((practice) => {
+    const { imageGallery, ...restOfPractice } = practice;
+    if (imageGallery) {
+      let formattedImageGallery = imageGallery.map(
+        (image) => `https://dryeyerescue-images.s3.amazonaws.com/${image}`
+      );
+      const newPractice = {
+        imageGallery: formattedImageGallery,
+        ...restOfPractice,
+      };
+      return newPractice;
+    }
+    return practice;
+  });
+
+  // Get all services
+  const servicesParams = {
+    TableName: process.env.services_and_treatments_table,
+  };
+  const servicesResponse = await dynamoDb.scan(servicesParams);
+  const services = servicesResponse.Items;
+
+  // Format Tests
+  formattedItems = formattedItems.map((practice) => {
+    const { tests, ...restOfPractice } = practice;
+    if (tests) {
+      let formattedTests = tests.map((test) => {
+        const testsList = services.filter((service) => service.type === "test");
+        const currentTest = testsList
+          ? testsList.filter((testItem) => testItem.id === test)
+          : [];
+        return currentTest && currentTest.length === 1
+          ? currentTest[0].label
+          : "";
+      });
+      const newPractice = {
+        tests: formattedTests,
+        ...restOfPractice,
+      };
+      return newPractice;
+    }
+    return practice;
+  });
+
+  // Format Treatments
+  formattedItems = formattedItems.map((practice) => {
+    const { dryEyeTreatments, ...restOfPractice } = practice;
+    if (dryEyeTreatments) {
+      let formattedTests = dryEyeTreatments.map((test) => {
+        const testsList = services.filter(
+          (service) => service.type === "treatment"
+        );
+        const currentTest = testsList
+          ? testsList.filter((testItem) => testItem.id === test)
+          : [];
+        return currentTest && currentTest.length === 1
+          ? currentTest[0].label
+          : "";
+      });
+      const newPractice = {
+        dryEyeTreatments: formattedTests,
+        ...restOfPractice,
+      };
+      return newPractice;
+    }
+    return practice;
+  });
+
+  // Format Services
+  formattedItems = formattedItems.map((practice) => {
+    const { eyeCareServices, ...restOfPractice } = practice;
+    if (eyeCareServices) {
+      let formattedTests = eyeCareServices.map((test) => {
+        const testsList = services.filter(
+          (service) => service.type === "service"
+        );
+        const currentTest = testsList
+          ? testsList.filter((testItem) => testItem.id === test)
+          : [];
+        return currentTest && currentTest.length === 1
+          ? currentTest[0].label
+          : "";
+      });
+      // filter out services
+      const filteredServices = formattedTests.filter(
+        (service) => service !== ""
+      );
+      const newPractice = {
+        eyeCareServices: filteredServices,
+        ...restOfPractice,
+      };
+      return newPractice;
+    }
+    return practice;
+  });
+
+  // Format Products
+  const productsRequest = await axios(
+    "https://wholesale.dryeyerescue.com/products.json?limit=500&page=1"
+  );
+  const products = productsRequest.data.products;
+  formattedItems = formattedItems.map((practice) => {
+    const { dryEyeProducts, ...restOfPractice } = practice;
+    if (
+      dryEyeProducts &&
+      typeof dryEyeProducts !== "string" &&
+      dryEyeProducts.length > 0
+    ) {
+      let formattedProducts = dryEyeProducts.map((product) => {
+        const currentProduct = products.filter((prod) => prod.id === product);
+        const currentFormattedProduct =
+          currentProduct && currentProduct.length === 1
+            ? currentProduct[0]
+            : null;
+        if (currentFormattedProduct) {
+          const { title, images, handle } = currentFormattedProduct;
+          return {
+            title,
+            image: images && images.length > 0 ? images[0].src : "",
+            handle: `https://dryeyerescue.com/products/${handle}`,
+          };
+        }
+        return null;
+      });
+      // filter out null values
+      const filteredProducts = formattedProducts.filter(
+        (prod) => prod !== null
+      );
+      const newPractice = {
+        dryEyeProducts: filteredProducts,
+        ...restOfPractice,
+      };
+      return newPractice;
+    }
+    return practice;
+  });
+
+  return formattedItems;
+}, true);
